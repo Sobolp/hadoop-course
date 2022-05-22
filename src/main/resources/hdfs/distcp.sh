d7f96d2dcf0b0e7e36aa56e51cc189aa559cc674
VOpolskiy <vadopolski@gmail.com>
2022-05-22 23:17:41 +0200

    Add map-reduce wordcount

--- src/main/resources/hdfs/distcp.sh
+++ src/main/resources/hdfs/distcp.sh
@@ -17,6 +17,15 @@ hdfs dfs -ls /user/vopolskiy_csv
 
 # start the command
 hadoop distcp -D dfs.replication=2 -update s3://spark-luxoft/london_crimes/london_crime_by_lsoa.csv /user/vopolskiy_csv
+
+
+hadoop distcp s3://spark-luxoft/map_reduce/WordCount.jar /map_reduce/jar2
+
+hdfs dfs -copyFromLocal -f /opt/hadoop-3.2.1/README.txt /map_reduce/input3
+
+
+hadoop distcp s3://spark-luxoft/map_reduce/README.md /map_reduce/input
+
 hadoop distcp -D dfs.blocksize=268435456 -update s3://spark-luxoft/london_crimes/london_crime_by_lsoa.csv /user/vopolskiy_csv
 
 
