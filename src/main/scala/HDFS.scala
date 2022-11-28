a1df1fcdc85a21fa08d2ef62f0384e1439dff0ff
PBolotov <pbolotov@luxoft.com>
2022-11-28 14:17:59 -0500

    my implementation

--- src/main/scala/HDFS.scala
+++ src/main/scala/HDFS.scala
@@ -9,31 +9,34 @@ import org.apache.hadoop.io.IOUtils
 object HDFS extends App {
   val conf = new Configuration()
   val hdfs: FileSystem = FileSystem.get(conf)
-  val fromPath = new Path("/")
-  val listStatus = hdfs.listStatus(fromPath)
-  listStatus.filter(_.isDirectory).map(_.getPath)
-    .foreach{ path =>
-//      1 create new path in ods target folder if not exists
-//      2 create new file in new folder if not exists or if file size reaches the limit
-//      3 adding nested loop  & add filtering for .inprogress
-//      4 in nested loop read and append all files from the path
-//      close the resoures
+  val fromPath = new Path("/user/stage/")
+  val listOfFolders = hdfs.listStatus(fromPath)
 
-      //
-//      add logs
-//      add config
-//      add try catch
-//      variab
+  def mergeIntoOnePart(path: String): Unit = {
+    val targetPath = path.replaceFirst("stage", "pbolotov/ods")
+    //      println(targetPath)
+    hdfs.mkdirs(new Path(targetPath))
+
+    val target = new Path(s"$targetPath/part-0000.csv")
+    if (!hdfs.exists(target)) hdfs.createNewFile(target)
+    if (hdfs.getStatus(target).getCapacity > 0) {
+      val fileOutputStream = hdfs.append(target)
+
+      val listOfFiles: Array[FileStatus] = hdfs.listStatus(new Path(path))
+      listOfFiles.
+        map(_.getPath.toString).
+        filter(_.endsWith(".csv")).
+        foreach(sf => {
+          println(sf)
+          val fileInputStream = hdfs.open(new Path(sf))
+          IOUtils.copyBytes(fileInputStream, fileOutputStream, 4096, false)
+          fileInputStream.close()
+        })
+      println()
+      fileOutputStream.close()
     }
+  }
 
-  val newFile = s"/user/vopolski/ods/part-0000.csv"
-  val newPath = new Path(newFile)
+  listOfFolders.map(_.getPath.toString).map(mergeIntoOnePart);
 
-  if (!hdfs.exists(newPath)) hdfs.createNewFile(newPath)
-
-  val inputStream  = hdfs.open(new Path("/user/stage/date=2020-12-03/part-0000.csv"))
-  val outputStream = hdfs.append(newPath)
-  IOUtils.copyBytes(inputStream, outputStream, 4096, true)
-  inputStream.close()
-  outputStream.close()
 }
