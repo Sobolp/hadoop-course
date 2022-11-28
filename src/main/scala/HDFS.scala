8caa62d478668f42579a95792eec977a6bce6e5e
VOpolskiy <vadopolski@gmail.com>
2022-11-28 17:22:07 +0100

    Added 2 Added correct scala code

--- src/main/scala/HDFS.scala
+++ src/main/scala/HDFS.scala
@@ -13,10 +13,14 @@ object HDFS extends App {
   val listStatus = hdfs.listStatus(fromPath)
   listStatus.filter(_.isDirectory).map(_.getPath).foreach(println)
 
-  val fileOutputStream = hdfs.create(new Path("/user/ods/date=2020-12-03/part-0000.csv"))
-  val fileInputStream = hdfs.open(new Path("/user/stage/date=2020-12-03/part-0000.csv"))
-  IOUtils.copyBytes(fileInputStream, fileOutputStream, 4096, true)
+  val newFile = s"/user/vopolski/ods/part-0000.csv"
+  val newPath = new Path(newFile)
 
-  fileOutputStream.close()
-  fileInputStream.close()
+  if (!hdfs.exists(newPath)) hdfs.createNewFile(newPath)
+
+  val inputStream  = hdfs.open(new Path("/user/stage/date=2020-12-03/part-0000.csv"))
+  val outputStream = hdfs.append(newPath)
+  IOUtils.copyBytes(inputStream, outputStream, 4096, true)
+  inputStream.close()
+  outputStream.close()
 }
